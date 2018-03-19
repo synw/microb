c6c34ef928324038ea117fd00adfa9a3d2093f07
Win <synwe@yahoo.com>
2018-03-19 12:58:21 +0000

    Minor fixes

--- services/manifest.go
+++ services/manifest.go
@@ -1,12 +1,12 @@
 package services
 
 import (
-	//http "github.com/synw/microb-http/manifest"
+	http "github.com/synw/microb-http/manifest"
 	"github.com/synw/microb/libmicrob/types"
 	infos "github.com/synw/microb/services/infos"
 )
 
 var Services = map[string]*types.Service{
 	"infos": infos.Service,
-	//"http": http.Service,
+	"http":  http.Service,
 }
